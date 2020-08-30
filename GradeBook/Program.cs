39e05f1b0b07f6202956040cc782aec176313f51
Umesh Rai <Umesh.Rai508@outlook.com>
2020-08-30 08:22:28 +0100

    Add project files.

---
+++ GradeBook/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = 0.0;
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+
+            foreach (var grade in grades)
+            {
+                result += grade;
+            }
+            result /= grades.Count;
+
+        }
+    }
+}
